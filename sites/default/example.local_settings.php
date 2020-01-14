6a06a2ae8a332c42e083ade7f580d3661c16d3cd
b8mission <b8mission@gmail.com>
2020-01-14 13:10:14 +0200

    init commit

---
+++ sites/default/example.local_settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupalpass',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['config_sync_directory'] = 'sites/default/files/config_0iH7HBC0ZfDSciRFRuOGRcbHc84UoFB_V0S_9Ms8KaF1luTPkzKoJ1h2MrwpPVPZVuUZPrQ4NQ/sync';
