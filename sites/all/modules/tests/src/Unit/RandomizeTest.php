63a216d8059eceb032d812e793c2db9595ba6299
b8mission <b8mission@gmail.com>
2020-01-23 23:38:29 +0200

    RandomPageController codstyle fixes; UnitTest skeleton

---
+++ sites/all/modules/tests/src/Unit/RandomizeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Tests\random_module\Unit;
+
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests generation of ice cream.
+ *
+ * @group random_module
+ */
+class RandomizeTest extends UnitTestCase {
+
+  public function testSomething() {
+
+  }
+
+}
